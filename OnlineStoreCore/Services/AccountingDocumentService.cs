d97a79b4e2c7093376ab3fa36b0bd4e561d774c4
zohrejojam <zohrejojam297@gmail.com>
2020-08-10 11:30:00 +0430

    متد جدید برای سورت

--- OnlineStoreCore/Services/AccountingDocumentService.cs
+++ OnlineStoreCore/Services/AccountingDocumentService.cs
@@ -10,26 +10,28 @@ namespace OnlineStoreCore.Services
     public class AccountingDocumentService : IAccountingDocumentService
     {
         private readonly DataBaseContext DbContext;
+
         public AccountingDocumentService(DataBaseContext context)
         {
             DbContext = context;
         }
+
         public string CreateDocumentNumber()
         {
-            return DateTime.Now.ToString("yyyyMMdd") + 
-                   DateTime.Now.Hour.ToString() + 
-                   DateTime.Now.Minute.ToString() + 
+            return DateTime.Now.ToString("yyyyMMdd") +
+                   DateTime.Now.Hour.ToString() +
+                   DateTime.Now.Minute.ToString() +
                    DateTime.Now.Second.ToString();
         }
 
         public IList<MaterialListDto> GetStoreHouseInventory(int? page, int? count, string filter, string sortColumns)
         {
-            var takePage = page ?? 1;
-            var takeCount = count ?? 5;
+            int takePage = page ?? 1;
+            int takeCount = count ?? 5;
 
-            var query = DbContext.Products.Include(p=>p.Warehouse);
+            var query = DbContext.Products.Include(p => p.Warehouse);
 
-            List<MaterialListDto> result = query
+            IQueryable<MaterialListDto> result = query
                .Select(p => new MaterialListDto
                {
                    Id = p.WareHouseId,
@@ -40,87 +42,37 @@ namespace OnlineStoreCore.Services
                    Count = p.Warehouse.Count,
                    Status = p.Warehouse.Count == 0 ? "ناموجود" : (p.Warehouse.Count <= p.MinimumInventory ? "آماده سفارش" : "عادی")
 
-               })
-               .OrderBy(p => p.Id)
-               .Skip((takePage - 1) * takeCount)
-                 .Take(takeCount).ToList();
+               });
+            result = this.Paging(result, takePage, takeCount);
             result = this.FilterList(result, filter);
             result = this.SortList(result, sortColumns);
             return result.ToList();
-
         }
 
-        public List<MaterialListDto> FilterList(List<MaterialListDto> query, string filterString)
+        public IQueryable<MaterialListDto> Paging(IQueryable<MaterialListDto> query, int page, int count)
         {
-            List<MaterialListDto> result=new List<MaterialListDto>();
+            return query.OrderBy(p => p.Id)
+               .Skip((page - 1) * count)
+                 .Take(count);
+        }
+
+        public IQueryable<MaterialListDto> SortList(IQueryable<MaterialListDto> query, string SortColumnName)
+        {
+            return Extensions.OrderBy<MaterialListDto>(query, SortColumnName, true);
+        }
+
+        public IQueryable<MaterialListDto> FilterList(IQueryable<MaterialListDto> query, string filterString)
+        {
+
             filterString = filterString != "" && filterString != null ? filterString.ToLower().Trim() : "";
             if (filterString != "" && filterString != null)
-            {
-                result = query.Where(p => p.MaterialCode.ToLower().Contains(filterString) ||
-                                    p.MaterialTitle.ToLower().Contains(filterString) ||
-                                    p.MaterialGroupName.ToLower().Contains(filterString) ||
-                                    p.MinInventory.ToString() == filterString ||
-                                    p.Count.ToString() == filterString).ToList();
-            }
-            return result;
+                throw new Exception(Resources.Messages.ErrorOccured);
+
+            return query.Where(p => p.MaterialCode.ToLower().Contains(filterString) ||
+                                 p.MaterialTitle.ToLower().Contains(filterString) ||
+                                 p.MaterialGroupName.ToLower().Contains(filterString) ||
+                                 p.MinInventory.ToString() == filterString ||
+                                 p.Count.ToString() == filterString);
         }
-
-        public List<MaterialListDto> SortList(List<MaterialListDto> query, string SortColumnName)
-        {
-            SortColumnName = SortColumnName != "" && SortColumnName != null ? SortColumnName.ToLower().Trim() : "";
-            if (SortColumnName != "" && SortColumnName != null)
-            {
-                string[] sortList = SortColumnName.Split(',');
-                foreach (string item in sortList)
-                {
-                    switch (item)
-                    {
-                        case "materialcode":
-                            query = query.OrderBy(p => p.MaterialCode).ToList();
-                            break;
-
-                        case "materialcode_desc":
-                            query = query.OrderByDescending(p => p.MaterialCode).ToList();
-                            break;
-
-                        case "materialtitle":
-                            query = query.OrderBy(p => p.MaterialTitle).ToList();
-                            break;
-
-                        case "materialtitle_desc":
-                            query = query.OrderByDescending(p => p.MaterialTitle).ToList();
-                            break;
-
-                        case "materialgroupname":
-                            query = query.OrderBy(p => p.MaterialGroupName).ToList();
-                            break;
-
-                        case "materialgroupname_desc":
-                            query = query.OrderByDescending(p => p.MaterialGroupName).ToList();
-                            break;
-
-                        case "mininventory":
-                            query = query.OrderBy(p => p.MinInventory).ToList();
-                            break;
-
-                        case "mininventory_desc":
-                            query = query.OrderByDescending(p => p.MinInventory).ToList();
-                            break;
-
-                        case "count":
-                            query = query.OrderBy(p => p.Count).ToList();
-                            break;
-
-                        case "count_desc":
-                            query = query.OrderByDescending(p => p.Count).ToList();
-                            break;
-                        default:
-                            break;
-                    }
-                }
-            }
-            return query.ToList();
-        }
-
     }
 }
